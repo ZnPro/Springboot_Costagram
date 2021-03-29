317b998581a94dd84dbd31a02b5b6b2fa8404fba
ZnPro <kjch1611@naver.com>
2021-03-29 15:55:38 +0900

    12. costagram_profile.jsp 의 팔로우 카운트, 팔로우 유무, 게시글 개수를 위한UserProfileRespDto 완성

--- src/main/java/com/cos/costagram/web/UserController.java
+++ src/main/java/com/cos/costagram/web/UserController.java
@@ -1,15 +1,31 @@
 package com.cos.costagram.web;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.costagram.config.auth.PrincipalDetails;
+import com.cos.costagram.service.UserService;
+import com.cos.costagram.web.dto.user.UserProfileRespDto;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class UserController {
 	
+	private final UserService userService;
+	
 	@GetMapping("/user/{id}")
-	public String profile(@PathVariable int id) {
+	//security.core.annotation
+	public String profile(@PathVariable int id, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		
+		UserProfileRespDto userProfileRespDto = userService.회원프로필(id, principalDetails.getUser().getId());
+		model.addAttribute("dto", userProfileRespDto);
+		
 		return "user/profile";
 	}
 	
